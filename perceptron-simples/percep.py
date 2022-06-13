be8a2002593175c6cdc534068958ecdba5d27922
gabrielhom <gabriel.henriqueomello@gmail.com>
2022-06-13 20:50:22 -0300

    perceptron simples

--- perceptron-simples/percep.py
+++ perceptron-simples/percep.py
@@ -1,14 +1,15 @@
 pesos = [0, 0]
 
-x = [[0,0], [0,1], [1,0], [1,1]]
+x = [[0, 0], [0, 1], [1, 0], [1, 1]]
 
-y = [-1, -1, -1, 1] # saida and
+y = [-1, -1, -1, 1]  # saida and
+
 epoch = 100
 eta = 0.5
 bias = 0
 
 for _ in range(epoch):
-    pesos_ant = pesos.copy()
+    erros = 0
     for i in range(len(x)):
         soma = 0
         for j in range(len(x[i])):
@@ -17,16 +18,14 @@ for _ in range(epoch):
         saida = 1 if soma >= 0 else -1
         erro = y[i] - saida
         if saida != y[i]:
+            erros += 1
             for j in range(len(pesos)):
                 pesos[j] += eta * x[i][j]
             bias += erro
-    igual = True
-    for j in range(len(pesos)):
-        if pesos_ant[j] != pesos[j]:
-            igual = False
-    if igual:
+    if erros == 0:
         print(f"iterações treino: {_}")
         break
+
 
 def teste(x):
     soma = 0
@@ -35,8 +34,8 @@ def teste(x):
     soma += bias
     return 1 if soma >= 0 else -1
 
-print("pesos:")
-print(pesos)
+
+print(f"pesos: {pesos}")
 print(f"bias: {bias}")
 print("testes:")
 for entrada in x:
