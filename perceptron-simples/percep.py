0cb1b731cf92efbab0336d23ca6bab29cc03f365
gabrielhom <gabriel.henriqueomello@gmail.com>
2022-06-15 21:59:39 -0300

    atualizando perceptron

--- perceptron-simples/percep.py
+++ perceptron-simples/percep.py
@@ -1,12 +1,11 @@
-pesos = [0, 0]
+pesos = [0, 0, 0, 0]
 
-x = [[0, 0], [0, 1], [1, 0], [1, 1]]
+x = [[1, 0, 0, 0], [1, 1, 0, 0], [1, 1, 1, 0], [1, 0, 1, 1], [1, 1, 0, 1], [1, 1,1,1]]
 
-y = [-1, -1, -1, 1]  # saida and
+y = [1, 1, 1, 1, -1, -1]  # saida and
 
 epoch = 100 # maximo épocas
-eta = 0.5 # taxa de aprendizagem
-bias = 0 # vies de treinamento
+eta = 0.2 # taxa de aprendizagem
 
 for _ in range(epoch):
     erros = 0
@@ -14,14 +13,12 @@ for _ in range(epoch):
         soma = 0
         for j in range(len(x[i])):
             soma += x[i][j] * pesos[j]
-        soma += bias
         saida = 1 if soma > 0 else -1
         erro = y[i] - saida
         if saida != y[i]:
             erros += 1
             for j in range(len(pesos)):
                 pesos[j] += eta * x[i][j] * erro
-            bias += erro
     if erros == 0:
         print(f"iterações treino: {_}")
         break
@@ -31,12 +28,11 @@ def teste(x):
     soma = 0
     for i in range(len(x)):
         soma += x[i] * pesos[i]
-    soma += bias
-    return 1 if soma >= 0 else -1
+    return 1 if soma > 0 else -1
 
 
 print(f"pesos: {pesos}")
-print(f"bias: {bias}")
+print(f"bias: {pesos[0]}")
 print("testes:")
 for entrada in x:
     print(f"x = {entrada}, saida = {teste(entrada)}")
