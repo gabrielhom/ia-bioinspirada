172bbf5e681e6d282dc30f2c272acb90309e5485
gabrielhom <gabriel.henriqueomello@gmail.com>
2022-06-22 14:57:19 -0300

    scikit

---
+++ mlp/classification.py
@@ -0,0 +1,11 @@
+from sklearn.neural_network import MLPClassifier
+
+X = [[0., 0.], [1., 1.], [1., 0.], [0., 1.]]
+y = [0, 0, 1, 1]
+
+clf = MLPClassifier(solver='lbfgs', alpha=1e-5,
+                    hidden_layer_sizes=(5,2), random_state=1)
+
+clf.fit(X, y)
+
+print(clf.predict([[1., 0.], [0., 0.]]))
